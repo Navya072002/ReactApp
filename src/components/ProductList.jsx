cc662ffee69b385e452f7954a763a9254272edc6
navyaarora <navya7oct@gmail.com>
2023-09-25 11:53:17 +0530

    added api functionality

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,39 +1,41 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-
-const api_url = '';        //add url;
+import {faker} from '@faker-js/faker';
+import axios from 'axios'; 
 
 const ProductCardStyles = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    width: 300px;
-    height: 400px;
+    width: 380px;
+    height: 600px;
     border: 1px solid #ccc;
     margin: 20px;
     padding: 20px;
 
     img {
-        width: 100%;
-        height: 200px;
+        width: 300px;
+        height: 400px;
         object-fit: cover;
     }
     
     h3 {
+        color: black;
         margin: 10px 0;
     }
 
     p{
+        color: black;
         margin  : 10px  0;
     }
 `;
 
-function ProductCard({ product }){
+function ProductCard({product}){
     return (
         <ProductCardStyles>
-            <img src={product.imageUrl} alt={product.name}/>
-            <h3>{product.name}</h3>
-            <p>{product.description}</p>
+            <img src={faker.image.url()} alt={product.productName}/>
+            <h3>{product.productName}</h3>
+            <p>{product.productDescription}</p>
             <p>${product.price}</p>
             <button> Add to Cart</button>
         </ProductCardStyles>
@@ -43,14 +45,20 @@ function ProductCard({ product }){
 function ProductList() {
     const [products, setProducts] = useState([]);
     useEffect(() => {
-        fetch(api_url)
-            .then(response => response.json())
-            .then(data => setProducts(data));
+        const fakeProducts = Array.from({ length: 10 }, () => ({
+            id: faker.commerce.isbn,
+            image: faker.image.url(),
+            productName: faker.commerce.productName(),
+            productDescription: faker.commerce.productDescription(),
+            price: faker.commerce.price(),
+        }));
+        
+        setProducts(fakeProducts);
     }, []);
 
     return(
-        <div>
-            {products.map(product =>(
+        <div style={{display:"flex", flexWrap:"wrap"}}>
+            {products.map (product =>(
                 <ProductCard key={product.id} product={product}/>
             ))}
         </div>
